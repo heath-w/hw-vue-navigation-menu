39b22216792248c80060d2fdd56a91a5a21a3b4f
Heath Williams <awilli01@amgen.com>
2017-10-03 11:51:08 -0400

    added bind for class and controls for changing

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,46 @@
 var menu = new Vue( {
 	el : '#menu',
 	data : {
+		homeActive : false,
+		projectsActive : false,
+		servicesActive : false,
+		contactActive : false,
 		message : 'Please click a menu item'
 	},
 	methods : {
+		makeActive : function( item, label ) {
+			console.log( 'makeActive' );
+			console.log( 'makeActive - item[', item, '] label[', label, ']' );
 
+			// Remove active class from the previous anchor
+			this.homeActive = false;
+			this.projectsActive = false;
+			this.servicesActive = false;
+			this.contactActive = false;
+
+			// Add active class to the select anchor (item)
+			switch( item ) {
+				case 'homeMenu':
+					// console.log( 'switch homeMenu' );
+					this.homeActive = true;
+					break;
+				case 'projectsMenu':
+					// console.log( 'switch projectsMenu' );
+					this.projectsActive = true;
+					break;
+				case 'servicesMenu':
+					// console.log( 'switch homeMenu' );
+					this.servicesActive = true;
+					break;
+				case 'contactMenu':
+					// console.log( 'switch homeMenu' );
+					this.contactActive = true;
+					break;			
+
+			}
+
+			// Change the text of message
+			this.message = 'You chose ' + label;
+		}
 	}
 });
