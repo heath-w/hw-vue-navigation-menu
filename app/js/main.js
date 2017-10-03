9ab79f85cbe1e286f073414e4be07fb5acbc8e39
Heath Williams <awilli01@amgen.com>
2017-10-03 14:04:15 -0400

    Cleanup

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,12 @@ var menu = new Vue( {
 	},
 	methods : {
 		makeActive : function( item ) {
-			console.log( 'makeActive' );
-			console.log( 'makeActive - item[', item, '] old active[', this.active, ']' );
+			// console.log( 'makeActive' );
+			// console.log( 'makeActive - item[', item, '] old active[', this.active, ']' );
 
 			this.active = item;
 
-			console.log( 'makeActive - item[', item, '] new active[', this.active, ']' );
+			// console.log( 'makeActive - item[', item, '] new active[', this.active, ']' );
 		}
 	}
 });
